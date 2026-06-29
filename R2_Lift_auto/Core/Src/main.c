7d3e9ddb6ccf777cf337ae73c6ea9e58e4b7e6e9
ciyue YE <1581525057@qq.com>
2026-06-29 16:56:43 +0800

    一区二区已经好了

--- R2_Lift_auto/Core/Src/main.c
+++ R2_Lift_auto/Core/Src/main.c
@@ -77,7 +77,7 @@ int main(void)
   /* USER CODE END 1 */
 
   /* MPU Configuration--------------------------------------------------------*/
-  MPU_Config();
+b  MPU_Config();
 
   /* MCU Configuration--------------------------------------------------------*/
 
